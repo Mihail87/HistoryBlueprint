8affa82770b343c9c3499e724f65bd4738af6ad3
Mihai Leonte <mihai.leonte@me.com>
2017-12-03 20:29:01 +0200

    Initial Commit

---
+++ History Blueprint/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  History Blueprint
+//
+//  Created by Mihai Leonte on 12/3/17.
+//  Copyright © 2017 Mihai Leonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
